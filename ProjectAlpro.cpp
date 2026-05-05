b3ed58f9ffd70a705333140aaa7a74fd4ca67565
IsnaZ23 <muham.naniz@gmail.com>
2026-05-05 15:25:40 +0700

    membikin alamat file

--- ProjectAlpro.cpp
+++ ProjectAlpro.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
 using namespace std;
+struct item{
+    string nama;
+    int jumlah;
+    int durability;
+};
 
-int main() {
+struct player{
+    char username[30];
+    item backpack[36];
     
+};
+
+player userAktif;
+int main() {
+    int pilih;
+    string namaplayer;
+    FILE *pf;
+
+    cout<<"===== MINECRAFT INVENTORY SYSTEM===== \n"
+        <<"1. Bikin Akun baru (buat tas baru) \n"
+        <<"2. Login Akun Lama (Isi inventory lama ) \n"
+        <<"Pilih (NO 1-2) : ";
+    cin>>pilih;
+    cin.ignore();
+    
+    cout<<"Masukkan Nama Player: ";
+    getline(cin, namaplayer);
+    string namaFile = namaplayer + ".dat";
+
+    switch (pilih){
+    case 1:
+        
+    break;
+    
+    default:
+        break;
+    }
+
     return 0;
 }
